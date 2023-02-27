b73a7ca8ae3cc498105560ae354095c3fb013673
gchin97 <graceschinn@gmail.com>
2023-02-27 21:10:56 +0900

    your first javascript casinnnnooo game

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const h1 = document.getElementById("click");
+const generationnumber = document.getElementById("generation-input");
+const numbervalue = document.getElementById("numbervalue");
+const playbutton = document.getElementById("playbutton");
+const description= document.getElementById("description");
+const result= document.getElementById("result");
 
-function onclick(){
+function drawNumber(event) { 
+  event.preventDefault();
+  const range = generationnumber.value;
+  const user_choice=numbervalue.value;
+  const pc_choice = Math.ceil(Math.random()*range);
 
-        h1.classList.toggle("active");
-    
+    description.innerText=`You chose: ${user_choice}, the machine chose: ${pc_choice}`;
+
+  if(user_choice == pc_choice){
+    result.innerText=`You won!`;
+  }else{    
+    result.innerText=`You lost!`;
+  }
 }
 
 
-h1.addEventListener("click", onclick);
+playbutton.addEventListener("click", drawNumber);
+
+
